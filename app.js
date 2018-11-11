b026a5eecc4a9c9c43b62a5d0855000c6bee58a3
repidus <repidus49@gmail.com>
2018-11-11 09:24:24 +0000

    feat: Let node can service static and dynamic web pages

--- app.js
+++ app.js
@@ -4,6 +4,32 @@ app.use(express.static('public'));
 app.get('/', function(req, res) {
 	res.send('Hello home page');
 });
+app.get('/dynamic', function(req, res) {
+	var lis = '';
+	for (var i = 0; i < 5; i++) {
+		lis += '<li>coding</li>';
+	}
+	var time = Date();
+	var output = `
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8" />
+		<title></title>
+	</head>
+	<body>
+		Hello, Dynamic!
+		<ul>
+			<li>coding</li>
+			<li>coding</li>
+			<li>coding</li>
+			<li>coding</li>
+			<li>coding</li>
+		</ul>
+	</body>
+	</html>`;
+	res.send(output);
+});
 app.get('/route', function(req, res) {
 	res.send('Hello Router, <img src="/palm_trees.jpg">');
 });
