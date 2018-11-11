81640e86ad4ef41bdf77e9ed4ab684de821d9710
repidus <repidus49@gmail.com>
2018-11-11 09:05:18 +0000

    feat: Let node can service static files

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 app.get('/', function(req, res) {
 	res.send('Hello home page');
+});
+app.get('/route', function(req, res) {
+	res.send('Hello Router, <img src="/palm_trees.jpg">');
 });
 app.get('/login', function(req, res) {
 	res.send('Login please');
