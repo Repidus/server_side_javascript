a38b3b026811df6257247d3a2daf428db14ee452
repidus <repidus49@gmail.com>
2018-11-16 09:10:59 +0000

    feat: Practice query string and semantic url in /topic

--- app.js
+++ app.js
@@ -4,12 +4,35 @@ app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
+
+app.get('/topic/:id', function(req, res) {
+	var topics = [
+		'JavaScript is ...',
+		'Node.js is ...',
+		'Express is ...'
+	];
+	var output = `
+		<a href="/topic/0">JavaScript</a><br>
+		<a href="/topic/1">Node.js</a><br>
+		<a href="/topic/2">Express</a><br>
+		<br>
+		${topics[req.params.id]}
+	`
+	res.send(output);
+});
+
+app.get('/topic/:id/:mode', function(req, res) {
+	res.send(req.params.id + ', ' + req.params.mode);
+});
+
 app.get('/template', function(req, res) {
 	res.render('temp', {time:Date(), title:'Jade'});
 });
+
 app.get('/', function(req, res) {
 	res.send('Hello home page');
 });
+
 app.get('/dynamic', function(req, res) {
 	var lis = '';
 	for (var i = 0; i < 5; i++) {
@@ -36,12 +59,15 @@ app.get('/dynamic', function(req, res) {
 	</html>`;
 	res.send(output);
 });
+
 app.get('/route', function(req, res) {
 	res.send('Hello Router, <img src="/palm_trees.jpg">');
 });
+
 app.get('/login', function(req, res) {
 	res.send('Login please');
 });
+
 app.listen(5900, function() {
 	console.log('Connected 5900 port!');
 });
